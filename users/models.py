0171928f1155f423e63b62fc678d29c350c68b79
PresentJay <presentj94@gmail.com>
2020-02-26 20:41:40 +0900

    adjusting branches

--- users/models.py
+++ users/models.py
@@ -3,11 +3,12 @@ from phone_field import PhoneField
 from django.db import models
 
 # Create your models here.
+
+
 class User(AbstractUser):
-    
-    name = models.CharField(max_length = 10)
+
+    name = models.CharField(max_length=10)
     address1 = models.CharField(max_length=1024)
     address2 = models.CharField(max_length=1024)
     home_number = PhoneField(blank=True, help_text='Contact home number')
     phone_number = PhoneField(blank=True, help_text='Contact phone number')
-    
